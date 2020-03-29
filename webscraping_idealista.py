9be6b579ff2f2f5998ae9b5bc82ce9b22ab4cb35
Diogo Braga <diogofiliperfbraga@gmail.com>
2020-03-29 14:40:49 +0100

    webscraping idealista, +sapo (com headers)

--- webscraping_idealista.py
+++ webscraping_idealista.py
@@ -15,7 +15,7 @@ headers = {
 
 # CSV Header
 df = pd.DataFrame([], columns=['Nome', ...])
-df.to_csv('dados_sapo.csv', index=False, header=True)
+df.to_csv('dados_idealista.csv', index=False, header=True)
 
 
 def parseHTML(url):  # Parsing Html
@@ -31,9 +31,6 @@ def parseHTML(url):  # Parsing Html
 
 def imovelFeatures(soup):  # Extração das features da págima dum imóvel
 
-    #nome = soup.find('h1', class_='detailPropertyTitle')
-    # print(nome.getText())
-
     return
 
 
@@ -42,38 +39,40 @@ def main():
     i = 1
 
     while(1):
-        # Url das páginas de apartamentos da 'Sapo.pt', aceder num ciclo às páginas existentes
-        htmlBraga = f"https://casa.sapo.pt/comprar-apartamentos/braga/?sa=3&pn={i}"
+        # Url das páginas da 'Era.pt', aceder num ciclo às páginas existentes
+        htmlIdealista = f"https://www.idealista.pt/comprar-casas/braga/pagina-{i}"
 
-        print(htmlBraga)
+        print(htmlIdealista)
 
         # Final das páginas
-        if i == 22:
+        if i == 3:
             break
 
         # Realizar o parsing da página associada ao iterador do ciclo
-        soup = parseHTML(htmlBraga)
+        soup = parseHTML(htmlIdealista)
 
-        # print(soup)
+        # Encontrar o link de cada imóvel presente na página, visto que a página esta dividida em 'article' entao é só aceder a cada um e sacar o href
+        # 'href=True' faz com que depois seja possível aceder a esse atributo, o que vai acontecer já em baixo
 
-        # Encontrar o link de cada imóvel presente na página, através do id associado
-        container = soup.find_all('div', class_="photoContainer")
+        container = soup.find_all('div', class_='item-info-container')
 
         table = []
 
         for item in container:
             table.append(
-                item.find('a', id=re.compile("MC_Property*"), href=True))
+                item.find('a', class_='item-link', href=True))
 
-        # Para cada imóvel da página, aceder ao href e adicionar 'https://casa.sapo.pt' no início para o url ficar a funcionar
+        # print(table)
+
+        # Para cada imóvel da página, aceder ao href e adicionar 'https://www.idealista.pt' no início para o url ficar a funcionar
         for tableCode in table:
-            tableCode['href'] = "https://casa.sapo.pt" + tableCode['href']
+            tableCode['href'] = "https://www.idealista.pt" + tableCode['href']
             # print(tableCode['href'])
 
         # Para cada link do imóvel presente no imóvel, ir buscar as features associadas a esse através do método imovelFeatures(imovel)
-        for tableCode in table:
-            imovel = parseHTML(tableCode['href'])
-            imovelFeatures(imovel)
+        # for tableCode in data:
+        #    imovel = parseHTML(tableCode['href'])
+        #    imovelFeatures(imovel)
 
         # Aumentar o iterador associado às páginas
         i = i+1
