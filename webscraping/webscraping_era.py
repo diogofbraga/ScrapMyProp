b1ba4d9225f567ed3369f941263137ec2e0d3c6c
Diogo Braga <diogofiliperfbraga@gmail.com>
2020-04-08 13:51:58 +0100

    Imovirtual -> Rua; Dados por data de extração;

--- webscraping/webscraping_era.py
+++ webscraping/webscraping_era.py
@@ -4,14 +4,40 @@ import requests
 import pandas as pd
 
 # CSV Header
-df = pd.DataFrame([], columns=['Nome', 'Nº Quartos', 'Nº Casas de Banho', 'Nº Estacionamentos', 'Área Útil',
-                               'Área Terreno', 'Finalidade', 'Tipo de Imóvel', 'Estado',
-                               'Preço de Venda', 'Distrito', 'Concelho', 'Freguesia', 'Referência',
-                               'Infraestruturas', 'Tipo de modalidade', 'Zona', 'Constituição',
-                               'Sistema de abastecimento de água', 'Caixilharia das Janelas',
-                               'Climatização', 'Domótica', 'Edifício', 'Garagem',
-                               'Vistas', 'Exposição Solar', 'Segurança'])
-df.to_csv('dados_era.csv', index=False, header=True)
+df = pd.DataFrame(
+    [],
+    columns=[
+        "Nome",
+        "Nº Quartos",
+        "Nº Casas de Banho",
+        "Nº Estacionamentos",
+        "Área Útil",
+        "Área Terreno",
+        "Finalidade",
+        "Tipo de Imóvel",
+        "Estado",
+        "Preço de Venda",
+        "Distrito",
+        "Concelho",
+        "Freguesia",
+        "Coordenadas",
+        "Referência",
+        "Infraestruturas",
+        "Tipo de modalidade",
+        "Zona",
+        "Constituição",
+        "Sistema de abastecimento de água",
+        "Caixilharia das Janelas",
+        "Climatização",
+        "Domótica",
+        "Edifício",
+        "Garagem",
+        "Vistas",
+        "Exposição Solar",
+        "Segurança",
+    ],
+)
+df.to_csv("dados_era.csv", index=False, header=True)
 
 
 def parseHTML(url):  # Parsing Html
@@ -19,7 +45,7 @@ def parseHTML(url):  # Parsing Html
 
     text_content = requests.get(text_url).text
 
-    text_soup = BeautifulSoup(text_content, 'lxml')
+    text_soup = BeautifulSoup(text_content, "lxml")
 
     return text_soup
 
@@ -28,88 +54,102 @@ def imovelFeatures(soup):  # Extração das features da págima dum imóvel
 
     # ------ Nome ------
     # Método de webscraping: procurar tags 'span' com a classe 'openSansR t18 cinza33 line_height150'
-    nome = soup.find('span', {'class': 'openSansR t18 cinza33 line_height150'})
+    nome = soup.find("span", {"class": "openSansR t18 cinza33 line_height150"})
     # Estas 'class's tendem sempre a ser únicas, daí dar para ir direto ao conteúdo que se quer, mas verificar sempre isso
 
     # ------ Bloco de características principais ------
     # Método de webscraping: procurar tags 'ul' com a classe 'bloco-caracteristicas'
-    bloco_caracteristicas = soup.find('ul', {'class': 'bloco-caracteristicas'})
+    bloco_caracteristicas = soup.find("ul", {"class": "bloco-caracteristicas"})
 
     features = {}
 
     # Método de webscraping: estas características estavam dentro de uma lista de 'span's, portanto tive que fazer uma abordagem diferente
     # O dicionário 'features' vai ter as caracteristicas que existem no bloco (a key será o "title" = spans[0]), e o 'value' vai ser o spans[1] associado a cada elemento
     if bloco_caracteristicas is not None:
-        for li in bloco_caracteristicas.find_all('li'):
-            key = li.find(
-                'span')['title']
-            value = li.find(
-                'span', {'class': 'num'})
+        for li in bloco_caracteristicas.find_all("li"):
+            key = li.find("span")["title"]
+            value = li.find("span", {"class": "num"})
             if value is not None:
                 features[key] = value.getText()
 
     # ------ Características adicionais ------
     # Método de webscraping: cada característica tinha um id associado, portanto estas características fui diretamente pelo id
-    finalidade = soup.find(
-        id="ctl00_ContentPlaceHolder1_lbl_imovel_show_finalidade")
+    finalidade = soup.find(id="ctl00_ContentPlaceHolder1_lbl_imovel_show_finalidade")
 
-    tipoDeImovel = soup.find(
-        id="ctl00_ContentPlaceHolder1_lbl_imovel_show_tipo_imovel")
+    tipoDeImovel = soup.find(id="ctl00_ContentPlaceHolder1_lbl_imovel_show_tipo_imovel")
 
-    estado = soup.find(
-        id="ctl00_ContentPlaceHolder1_lbl_imovel_show_estado")
+    estado = soup.find(id="ctl00_ContentPlaceHolder1_lbl_imovel_show_estado")
 
-    preco_venda = soup.find(
-        id="ctl00_ContentPlaceHolder1_lbl_imovel_show_preco_venda")
+    preco_venda = soup.find(id="ctl00_ContentPlaceHolder1_lbl_imovel_show_preco_venda")
 
-    distrito = soup.find(
-        id="ctl00_ContentPlaceHolder1_lbl_imovel_show_distrito")
+    distrito = soup.find(id="ctl00_ContentPlaceHolder1_lbl_imovel_show_distrito")
 
-    concelho = soup.find(
-        id="ctl00_ContentPlaceHolder1_lbl_imovel_show_concelho")
+    concelho = soup.find(id="ctl00_ContentPlaceHolder1_lbl_imovel_show_concelho")
 
-    freguesia = soup.find(
-        id="ctl00_ContentPlaceHolder1_lbl_imovel_show_freguesia")
+    freguesia = soup.find(id="ctl00_ContentPlaceHolder1_lbl_imovel_show_freguesia")
 
-    ref = soup.find(
-        id="ctl00_ContentPlaceHolder1_lbl_imovel_show_ref")
+    ref = soup.find(id="ctl00_ContentPlaceHolder1_lbl_imovel_show_ref")
 
     # ------ Bloco de características extra ------
     # Método de webscraping: procurar tags 'div' com a classe 'imovel_show_list_caracteristicas box'
-    box_caract = soup.find_all(
-        'div', {'class': 'imovel_show_list_caracteristicas box'})
+    box_caract = soup.find_all("div", {"class": "imovel_show_list_caracteristicas box"})
 
     # Método de webscraping: (semelhante ao método das características principais)
     for item in box_caract:
-        key = item.find(
-            'span', {'class': 'caracteristicas_titulo_nome'})
-        value = item.find(
-            'span', {'class': 'caracteristicas_descricao'})
+        key = item.find("span", {"class": "caracteristicas_titulo_nome"})
+        value = item.find("span", {"class": "caracteristicas_descricao"})
         features[key.getText()] = value.getText()
 
-    # ------ Adicionar a informação extraída ao csv ------
-    obj = df.append({'Nome': nome.text, 'Nº Quartos': features.get("Quartos", None), 'Nº Casas de Banho': features.get("C. Banho", None),
-                     'Nº Estacionamentos': features.get("Estacionamento", None), 'Área Útil': features.get("Área Útil", None),
-                     'Área Terreno': features.get("Área Terreno", None), 'Finalidade': finalidade.text,
-                     'Tipo de Imóvel': tipoDeImovel.text, 'Estado': estado.text, 'Preço de Venda': preco_venda.text,
-                     'Distrito': distrito.text, 'Concelho': concelho.text, 'Freguesia': freguesia.text, 'Referência': ref.text,
-                     'Infraestruturas': features.get("Infraestruturas", None), 'Tipo de modalidade': features.get("Tipo", None),
-                     'Zona': features.get("Zona", None), 'Constituição': features.get('Geral', None),
-                     'Sistema de abastecimento de água': features.get('Água', None), 'Caixilharia das Janelas': features.get('Caixilharia', None),
-                     'Climatização': features.get('Climatização', None), 'Domótica': features.get('Domótica', None),
-                     'Edifício': features.get('Edifício', None), 'Garagem': features.get('Garagem', None),
-                     'Vistas': features.get('Vistas', None), 'Exposição Solar': features.get('Exposição Solar', None),
-                     'Segurança': features.get('Segurança', None)
-                     }, ignore_index=True)
+    # ------ Coordenadas geográficas ------
+    mapa_holder = soup.find("div", class_="mapa-holder")
 
-    obj.to_csv('dados_era.csv', mode='a', header=False, index=False)
+    mapa_img = mapa_holder.find("img", class_="img_mapa", onclick=True)
+
+    coordinates = re.sub(r".*query=(.*?)\'.*", r"\1", mapa_img["onclick"])
+
+    # ------ Adicionar a informação extraída ao csv ------
+    obj = df.append(
+        {
+            "Nome": nome.text,
+            "Nº Quartos": features.get("Quartos", None),
+            "Nº Casas de Banho": features.get("C. Banho", None),
+            "Nº Estacionamentos": features.get("Estacionamento", None),
+            "Área Útil": features.get("Área Útil", None),
+            "Área Terreno": features.get("Área Terreno", None),
+            "Finalidade": finalidade.text,
+            "Tipo de Imóvel": tipoDeImovel.text,
+            "Estado": estado.text,
+            "Preço de Venda": preco_venda.text,
+            "Distrito": distrito.text,
+            "Concelho": concelho.text,
+            "Freguesia": freguesia.text,
+            "Coordenadas": coordinates,
+            "Referência": ref.text,
+            "Infraestruturas": features.get("Infraestruturas", None),
+            "Tipo de modalidade": features.get("Tipo", None),
+            "Zona": features.get("Zona", None),
+            "Constituição": features.get("Geral", None),
+            "Sistema de abastecimento de água": features.get("Água", None),
+            "Caixilharia das Janelas": features.get("Caixilharia", None),
+            "Climatização": features.get("Climatização", None),
+            "Domótica": features.get("Domótica", None),
+            "Edifício": features.get("Edifício", None),
+            "Garagem": features.get("Garagem", None),
+            "Vistas": features.get("Vistas", None),
+            "Exposição Solar": features.get("Exposição Solar", None),
+            "Segurança": features.get("Segurança", None),
+        },
+        ignore_index=True,
+    )
+
+    obj.to_csv("dados_era.csv", mode="a", header=False, index=False)
 
 
 def main():
 
     i = 1
 
-    while(1):
+    while 1:
 
         # Final das páginas
         if i == 49:
@@ -129,20 +169,24 @@ def main():
         # "ctl00_ContentPlaceHolder1_DataList_imoveis_ctl01_hpl_img_imovel"
         # então '[0123456789]*' trata disso
         # 'href=True' faz com que depois seja possível aceder a esse atributo, o que vai acontecer já em baixo
-        table = soup.find_all(id=re.compile(
-            "ctl00_ContentPlaceHolder1_DataList_imoveis_ctl[0123456789]*_hpl_img_imovel"), href=True)
+        table = soup.find_all(
+            id=re.compile(
+                "ctl00_ContentPlaceHolder1_DataList_imoveis_ctl[0123456789]*_hpl_img_imovel"
+            ),
+            href=True,
+        )
 
         # Para cada imóvel da página, aceder ao href e adicionar 'https://www.era.pt' no início para o url ficar a funcionar
         for tableCode in table:
-            tableCode['href'] = "https://www.era.pt" + tableCode['href']
+            tableCode["href"] = "https://www.era.pt" + tableCode["href"]
 
         # Para cada link do imóvel presente no imóvel, ir buscar as features associadas a esse através do método imovelFeatures(imovel)
         for tableCode in table:
-            imovel = parseHTML(tableCode['href'])
+            imovel = parseHTML(tableCode["href"])
             imovelFeatures(imovel)
 
         # Aumentar o iterador associado às páginas
-        i = i+1
+        i = i + 1
 
 
 main()
