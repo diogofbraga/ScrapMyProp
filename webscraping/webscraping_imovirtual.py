58ea64f9685e612ee1e7e035cda30fe265b33aa6
Diogo Braga <diogofiliperfbraga@gmail.com>
2020-04-21 12:36:24 +0100

    data 21/04

--- webscraping/webscraping_imovirtual.py
+++ webscraping/webscraping_imovirtual.py
@@ -440,7 +440,7 @@ def main():
     while 1:
 
         # Final das páginas
-        if i == 240:
+        if i == 225:
             break
 
         # Url das páginas de apartamentos da 'Sapo.pt', aceder num ciclo às páginas existentes
